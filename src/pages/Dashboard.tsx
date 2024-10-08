b83601a51e6a710b0c00a223ba81b137c947630b
fabiomaciel7 <fabio.maciel@ccc.ufcg.edu.br>
2024-10-08 12:39:17 -0300

    ajuste dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -107,6 +107,7 @@ const Dashboard: React.FC = () => {
           <h2>Minhas Tarefas</h2>
         </div>
 
+        <div className="content-header2">
         <Link to="/createTask" className="add-task-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
@@ -114,6 +115,7 @@ const Dashboard: React.FC = () => {
           </svg>
           Adicionar Tarefa
         </Link>
+        </div>
 
         <DragDropContext onDragEnd={handleOnDragEnd}>
           {tasks.length > 0 && (
